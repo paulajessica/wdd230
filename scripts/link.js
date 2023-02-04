0da39e9528b38fb078de91a1737c780ce27b9ce7
paulajessica <u811780803@byui.edu>
2023-02-03 22:16:42 -0300

    week05

--- scripts/link.js
+++ scripts/link.js
@@ -1,27 +1,25 @@
-const urla = 
-{
-    "week01":["week01/media-query.html", "week02/design.html"],
-    // "week02":["https://codepen.io/paulajessica/pen/ExpmMRp","week02/bom.html"],
-    // "week03":["week03/images/landscape..webp.webp","week03/lazyload.html","https://codepen.io/paulajessica/pen/jOpawPX","week03/bom.html"],
-    // "week04":["https://codepen.io/paulajessica/pen/oNMqaOm","images/exerciseweek04.png","week04/tablebuild.html","week04/form-start/index.html"],
-    // "week05":["https://codepen.io/paulajessica/pen/qByQdEG","week05/new-ward-members.json","week05/prophets.html"]
-};
-
-
 
 async function getData() {
-    const response = await fetch(urla);
+    const response = await fetch('./link.json');
     const data = await response.json();
-    console.table(data);
-    //displayData(data.prophets);
+    //console.log({ data });
+    //displayData(data[week]);
+    const section = document.querySelector(".cardactivities");
+    for (let week in data){
+        
+        const li = document.createElement("li");
+        li.innerHTML = week + ": ";
+        for (let link in data[week]){
+            //console.log(data[week][link]);
+            const alink = document.createElement("a");
+            alink.innerHTML = data[week][link].title;
+            alink.setAttribute("hrfe", data[week][link].path);
+            li.appendChild(alink);
+            li.innerHTML += " | ";
+        }
+        section.appendChild(li);
+        }
 }
 
 getData();
 
-const displayData = (datas) => {
-    datas.forEach((data) => {
-        const cards = document.querySelector('.cardactivities');
-        const a = document.querySelector('a');
-        a.href = data;
-    });
-  }
