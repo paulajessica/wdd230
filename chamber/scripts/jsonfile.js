bb91272ca299da304005f0cf2c494e63a1e55728
paulajessica <u811780803@byui.edu>
2023-02-12 20:34:14 -0300

    week06

--- chamber/scripts/jsonfile.js
+++ chamber/scripts/jsonfile.js
@@ -1,5 +1,20 @@
 const cards = document.querySelector('#cards');
 
+const nameOne = document.querySelector('#nameOne');
+const imgOne = document.querySelector('#imgOne');
+const webOne = document.querySelector('#webOne');
+const phoneOne = document.querySelector('#phoneOne');
+
+const nameTwo = document.querySelector('#nameTwo');
+const imgTwo = document.querySelector('#imgTwo');
+const webTwo = document.querySelector('#webTwo');
+const phoneTwo = document.querySelector('#phoneTwo');
+
+const nameThree = document.querySelector('#nameThree');
+const imgThree = document.querySelector('#imgThree');
+const webThree = document.querySelector('#webThree');
+const phoneThree = document.querySelector('#phoneThree');
+
 async function getData() {
     const response = await fetch('./members.json');
     const data = await response.json();
@@ -20,9 +35,6 @@ const displayCompany = (datas) => {
         const website = document.createElement("p");
         const level = document.createElement("p");
         const area = document.createElement("p");
-        
-        
-
         
         icon.setAttribute('src', data.icon);
         icon.setAttribute('alt', `Icon of ${data.name}`);
@@ -48,9 +60,60 @@ const displayCompany = (datas) => {
 
 
     });
-  }
+}
 
 
+async function getSpotlight() {
+    const response = await fetch('./members.json');
+    const datas = await response.json();
+    //console.log(nameOne.textContent = data["companies"][6].name);
+    //console.log(data["companies"][0].name);
+    const array = datas["companies"];
+
+    arrayRandom = [];
+    
+    array.forEach((data) => {
+        // console.log(data.level);
+        // console.log(index);
+    
+        let randomOne = Math.floor(Math.random() * array.length);
+        const levelOne  = array[randomOne].level;
+        
+        
+        if (!arrayRandom.includes(randomOne) && (levelOne == "Silver" || levelOne == "Gold")){
+            console.log("r1")
+            console.log(randomOne);
+            console.log(levelOne);
+            arrayRandom.push(randomOne);
+            nameOne.textContent = array[randomOne].name;
+            webOne.textContent = array[randomOne].website;
+            phoneOne.textContent = array[randomOne].phonenumber;
+            imgOne.setAttribute('src', array[randomOne].icon);            
+            
+        };
+        array.forEach((data) => {
+
+            let randomTwo = Math.floor(Math.random() * array.length);
+            const levelTwo  = array[randomOne].level;        
+            if (!arrayRandom.includes(randomTwo) && (levelTwo == "Silver" || levelTwo == "Gold")){
+                console.log("r2")
+                console.log(randomTwo);
+                console.log(levelTwo);
+                arrayRandom.push(randomTwo);
+                nameTwo.textContent = array[randomTwo].name;
+                webTwo.textContent = array[randomTwo].website;
+                phoneTwo.textContent = array[randomTwo].phonenumber;
+                imgTwo.setAttribute('src', array[randomTwo].icon);
+            }
+        
+            });
+            
+
+    });
+     
+};
+
+getSpotlight();
 
 
 
---
+++ chamber/scripts/weather.js
@@ -0,0 +1,129 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const description = document.querySelector('#description');
+
+const dateOne = document.querySelector('#dateOne');
+const iconOne = document.querySelector('#iconOne');
+const tempOne = document.querySelector('#tempOne');
+
+
+const dateTwo = document.querySelector('#dateTwo');
+const iconTwo = document.querySelector('#iconTwo');
+const tempTwo = document.querySelector('#tempTwo');
+
+
+const dateThree = document.querySelector('#dateThree');
+const iconThree = document.querySelector('#iconThree');
+const tempThree = document.querySelector('#tempThree');
+
+
+const tempFour = document.querySelector('#tempFour');
+const iconFour = document.querySelector('#iconFour');
+const datefour = document.querySelector('#datefour');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fortaleza&appid=1c81c5fdee81d7fcb6ad26931a8b040f';
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data.weather); // testing only
+        displayTemp(data.main);
+        displayWeather(data.weather);
+        displayDescription(data.weather);
+        
+               
+        } else {
+            throw Error(await response.text());
+        };
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+  
+apiFetch();
+
+function displayTemp(main){
+    const temp = main.temp;         
+    currentTemp.innerHTML = temp;
+
+};
+
+function displayTemp(main){
+    const temp = main.temp;         
+    currentTemp.innerHTML = `Time Today: ${temp}&deg;F`;
+
+};
+
+function displayDescription(weather){
+    weather.forEach((weather) => {
+        const desc = weather.description;
+        description.innerHTML = `Description Time: ${desc}`;
+        
+    });
+};
+
+function displayWeather(weather){
+    weather.forEach((weather) => {
+        const img = weather.icon;
+        weatherIcon.setAttribute('src', "http://openweathermap.org/img/w/" + img + ".png");
+        
+        
+    });
+};
+
+
+
+const urlforecast = 'https://api.openweathermap.org/data/2.5/forecast?lat=-38.527&lon=-3.7227&appid=1c81c5fdee81d7fcb6ad26931a8b040f'
+        //https://api.openweathermap.org/data/2.5/forecast?lat=-38.527&lon=-3.7227&appid=1c81c5fdee81d7fcb6ad26931a8b040f
+async function apiFetchFore() {
+        const response = await fetch(urlforecast);
+        const data = await response.json();
+        //console.log(data.list[17].weather[0].icon);            
+       // console.log(displayTime(list));       
+       //for (let i in data.list){
+
+       const dOne = data.list[9].dt_txt;
+       dateOne.innerHTML = `In: ${dOne}`;
+       const iOne = data.list[9].weather[0].icon;
+       iconOne.setAttribute('src', "http://openweathermap.org/img/w/" + iOne + ".png");           
+       const tOne = data.list[9].main.temp;
+       tempOne.innerHTML = `Time: ${tOne}&deg;F`;
+
+       const dTwo = data.list[17].dt_txt;
+       dateTwo.innerHTML = `In: ${dTwo}` ;
+       const iTwo = data.list[17].weather[0].icon;
+       iconTwo.setAttribute('src', "http://openweathermap.org/img/w/" + iTwo + ".png");  
+       const tTwo = data.list[17].main.temp;
+       tempTwo.innerHTML = `Time: ${tTwo}&deg;F`;
+
+       const dThree = data.list[25].dt_txt;
+       dateThree.innerHTML = `In: ${dThree}` ;
+       const iThree = data.list[25].weather[0].icon;
+       iconThree.setAttribute('src', "http://openweathermap.org/img/w/" + iThree + ".png"); 
+       const tThree = data.list[25].main.temp;
+       tempThree.innerHTML = `Time: ${tThree}&deg;F`;
+
+       const dFour = data.list[33].dt_txt;
+       dateFour.innerHTML = `In: ${dFour}`;
+       const iFour = data.list[33].weather[0].icon;
+       iconFour.setAttribute('src', "http://openweathermap.org/img/w/" + iFour + ".png");
+       const tFour = data.list[33].main.temp;
+       tempFour.innerHTML = `Time: ${tFour}&deg;F`;
+       
+};
+
+apiFetchFore();
+
+
+
+
+
+
+
+
+  
